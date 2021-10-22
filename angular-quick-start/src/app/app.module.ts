b4b5ce201ce4265627a35504df781a840df6c8b5
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2021-10-22 17:12:13 +0900

    FIX app.module.ts

--- angular-quick-start/src/app/app.module.ts
+++ angular-quick-start/src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Sample1Component } from './components/sample1component';
+import { Sample2Component } from './components/sample2/sample2.component';
+import { Sample3Component } from './components/sample3/sample3.component';
+import { Sample4Component } from './components/sample4/sample4.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, Sample1Component, Sample2Component, Sample3Component, Sample4Component],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
