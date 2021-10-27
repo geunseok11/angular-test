6b62c0df605aa187f1230177b45fd3e661f40909
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2021-10-27 23:28:58 +0900

    ADD components

---
+++ angular-quick-start/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { Sample1Component } from './components/sample1component';
+import { Sample2Component } from './components/sample2/sample2.component';
+import { Sample3Component } from './components/sample3/sample3.component';
+import { Sample4Component } from './components/sample4/sample4.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Sample1Component,
+    Sample2Component,
+    Sample3Component,
+    Sample4Component,
+  ],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ angular-quick-start/src/app/components/sample4/sample4.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sample4',
+  template: ` <p>sample4 works!</p> `,
+  styles: [],
+})
+export class Sample4Component implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
